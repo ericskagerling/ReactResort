3efffc9221edb559f695f892ffd38b9b611e1641
Your Name <you@example.com>
2026-08-14 10:15:36 +0200

    added weather function, but without geolocation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Hero } from "./components/Hero";
+import WeatherInfo from "./components/WeatherInfo";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <WeatherInfo />
     </>
   );
 }
