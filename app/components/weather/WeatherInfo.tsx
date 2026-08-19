107bf4d2dc237b6332b5782cff9bb46bd3f80f92
Your Name <you@example.com>
2026-08-19 15:01:35 +0200

    added black and white icons to weather app

--- app/components/weather/WeatherInfo.tsx
+++ app/components/weather/WeatherInfo.tsx
@@ -1,6 +1,14 @@
 "use client";
 import { useEffect, useState } from "react";
-
+import {
+  Sun,
+  CloudSun,
+  Cloud,
+  Umbrella,
+  Snowflake,
+  CloudRain,
+  Zap,
+} from "lucide-react";
 //Detta är vad vi får tillbaka från API:et, det används för att typa vårt state
 type WeatherData = {
   current: {
@@ -15,15 +23,77 @@ type WeatherInfoProps = {
 };
 
 // Funktion som tar emot en väder-kod och returnerar beskrivning av vädret
+// const weatherCodeDescription = (code: number) => {
+//   if (code === 0) return "Clear sky ☀";
+//   if (code === 1) return "Mainly clear 🌤️";
+//   if (code === 2) return "Partly cloudy ☀";
+//   if (code === 3) return "Overcast ☁️";
+//   if (code === 61 || code === 63 || code === 65) return "Rain 🌧️";
+//   if (code === 71 || code === 73 || code === 75) return "Snow fall ❄️";
+//   if (code === 80 || code === 81 || code === 82) return "Rain showers 🌦️";
+//   if (code === 95) return "Thunderstorm ⛈️";
+//   return "Unknown weather code";
+// };
 const weatherCodeDescription = (code: number) => {
-  if (code === 0) return "Clear sky ☀️";
-  if (code === 1) return "Mainly clear 🌤️";
-  if (code === 2) return "Partly cloudy ⛅";
-  if (code === 3) return "Overcast ☁️";
-  if (code === 61 || code === 63 || code === 65) return "Rain 🌧️";
-  if (code === 71 || code === 73 || code === 75) return "Snow fall ❄️";
-  if (code === 80 || code === 81 || code === 82) return "Rain showers 🌦️";
-  if (code === 95) return "Thunderstorm ⛈️";
+  const iconClass =
+    "inline-block h-5 w-5 ml-1  relative top-[-4px]  stroke-[1]";
+
+  if (code === 0)
+    return (
+      <>
+        Clear sky <Sun className={iconClass} />
+      </>
+    );
+
+  if (code === 1)
+    return (
+      <>
+        Mainly clear <Sun className={iconClass} />
+      </>
+    );
+
+  if (code === 2)
+    return (
+      <>
+        Partly cloudy <CloudSun className={iconClass} />
+      </>
+    );
+
+  if (code === 3)
+    return (
+      <>
+        Overcast <Cloud className={iconClass} />{" "}
+      </>
+    );
+
+  if (code === 61 || code === 63 || code === 65)
+    return (
+      <>
+        Rain <Umbrella className={iconClass} />{" "}
+      </>
+    );
+
+  if (code === 71 || code === 73 || code === 75)
+    return (
+      <>
+        Snow fall <Snowflake className={iconClass} />{" "}
+      </>
+    );
+
+  if (code === 80 || code === 81 || code === 82)
+    return (
+      <>
+        Rain showers <CloudRain className={iconClass} />{" "}
+      </>
+    );
+
+  if (code === 95)
+    return (
+      <>
+        Thunderstorm <Zap className={iconClass} />{" "}
+      </>
+    );
+
   return "Unknown weather code";
 };
 
